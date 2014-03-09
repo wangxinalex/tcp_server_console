eb67e53358c5b36308b7a29d7220774a3d182d20
wangxinalex <wangxinalex@gmail.com>
2014-03-09 14:38:56 +0800

    initialized

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netbar_manager_console {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
